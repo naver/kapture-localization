df6961999f58b289849374a57b5a68161ab46dd4
yohann.cabon <yohann.cabon@naverlabs.com>
2020-11-20 12:04:35 +0100

    remove tutorial folder

--- samples/virtual_gallery_tutorial/reset_tutorial_folder.py
+++ samples/virtual_gallery_tutorial/reset_tutorial_folder.py
@@ -6,10 +6,16 @@ import kapture_localization.utils.path_to_kapture  # noqa: F401
 from kapture.utils.paths import safe_remove_any_path
 
 HERE_PATH = path.normpath(path.dirname(__file__))
-tutorial_folder = path.join(HERE_PATH, 'tutorial')
+colmap_sfm_folder = path.join(HERE_PATH, 'colmap-sfm')
+colmap_localization_folder = path.join(HERE_PATH, 'colmap-localization')
+sift_colmap_vocab_tree_folder = path.join(HERE_PATH, 'sift_colmap_vocab_tree')
 
-if path.isdir(tutorial_folder):
-    safe_remove_any_path(tutorial_folder, force=False)
+if path.isdir(colmap_sfm_folder):
+    safe_remove_any_path(colmap_sfm_folder, force=False)
+if path.isdir(colmap_localization_folder):
+    safe_remove_any_path(colmap_localization_folder, force=False)
+if path.isdir(sift_colmap_vocab_tree_folder):
+    safe_remove_any_path(sift_colmap_vocab_tree_folder, force=False)
 
 matches_no_gv_folder = path.join(HERE_PATH, 'local_features/r2d2_500/NN_no_gv')
 if path.isdir(matches_no_gv_folder):
