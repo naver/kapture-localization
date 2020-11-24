929588399ce3095d7feffc0913b9a223dd896533
yohann.cabon <yohann.cabon@naverlabs.com>
2020-11-24 10:22:02 +0100

    added irbench tuto folder to reset script

--- samples/virtual_gallery_tutorial/reset_tutorial_folder.py
+++ samples/virtual_gallery_tutorial/reset_tutorial_folder.py
@@ -9,6 +9,7 @@ HERE_PATH = path.normpath(path.dirname(__file__))
 colmap_sfm_folder = path.join(HERE_PATH, 'colmap-sfm')
 colmap_localization_folder = path.join(HERE_PATH, 'colmap-localization')
 sift_colmap_vocab_tree_folder = path.join(HERE_PATH, 'sift_colmap_vocab_tree')
+ir_bench_folder = path.join(HERE_PATH, 'image_retrieval_benchmark')
 
 if path.isdir(colmap_sfm_folder):
     safe_remove_any_path(colmap_sfm_folder, force=False)
@@ -16,6 +17,8 @@ if path.isdir(colmap_localization_folder):
     safe_remove_any_path(colmap_localization_folder, force=False)
 if path.isdir(sift_colmap_vocab_tree_folder):
     safe_remove_any_path(sift_colmap_vocab_tree_folder, force=False)
+if path.isdir(ir_bench_folder):
+    safe_remove_any_path(ir_bench_folder, force=False)
 
 matches_no_gv_folder = path.join(HERE_PATH, 'local_features/r2d2_500/NN_no_gv')
 if path.isdir(matches_no_gv_folder):
