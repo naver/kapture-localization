cbbbc8c39d4b644223e262a402fc2da7ccd81ee9
Nicolas Guerin <nicolas.guerin@naverlabs.com>
2021-05-25 23:59:25 +0200

    Update version number. Fix Pypi installation.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ long_description = read_doc(readme_filepath)
 setuptools.setup(
     # description
     name='kapture-localization',
-    version="0.1.0",
+    version="0.1.1",
     author="naverlabs",
     author_email="kapture@naverlabs.com",
     description="kapture-localization: localization pipelines based on kapture.",
