6e87ba504e8969a487d86d4a81439937029b2cf7
mhumenbe <martin.humenberger@naverlabs.com>
2021-08-13 18:06:49 +0200

    changed RIO10 example to use testing instead of validation

--- pipeline/examples/run_rio10.sh
+++ pipeline/examples/run_rio10.sh
@@ -51,32 +51,32 @@ for SCENE in scene01 scene02 scene03 scene04 scene05 scene06 scene07 scene08 sce
   cp ${DATASETS_PATH}/${DATASET}/mapping/sensors/*.txt ${WORKING_DIR}/${DATASET}/mapping/sensors/
   ln -s ${DATASETS_PATH}/${DATASET}/mapping/sensors/records_data ${WORKING_DIR}/${DATASET}/mapping/sensors/records_data
 
-  mkdir -p ${WORKING_DIR}/${DATASET}/validation/sensors
-  cp ${DATASETS_PATH}/${DATASET}/validation/sensors/*.txt ${WORKING_DIR}/${DATASET}/validation/sensors/
-  ln -s ${DATASETS_PATH}/${DATASET}/validation/sensors/records_data ${WORKING_DIR}/${DATASET}/validation/sensors/records_data
+  mkdir -p ${WORKING_DIR}/${DATASET}/testing/sensors
+  cp ${DATASETS_PATH}/${DATASET}/testing/sensors/*.txt ${WORKING_DIR}/${DATASET}/testing/sensors/
+  ln -s ${DATASETS_PATH}/${DATASET}/testing/sensors/records_data ${WORKING_DIR}/${DATASET}/testing/sensors/records_data
 
-  # 2) Merge mapping and validation kaptures (this will make it easier to extract the local and global features and it will be used for the localization step)
+  # 2) Merge mapping and testing kaptures (this will make it easier to extract the local and global features and it will be used for the localization step)
   kapture_merge.py -v debug -f \
-    -i ${WORKING_DIR}/${DATASET}/mapping ${WORKING_DIR}/${DATASET}/validation \
-    -o ${WORKING_DIR}/${DATASET}/map_plus_validation \
+    -i ${WORKING_DIR}/${DATASET}/mapping ${WORKING_DIR}/${DATASET}/testing \
+    -o ${WORKING_DIR}/${DATASET}/map_plus_testing \
     --image_transfer link_relative
 
   # 3) Extract global features (we will use AP-GeM here)
   cd ${WORKING_DIR}/deep-image-retrieval
-  ${PYTHONBIN} -m dirtorch.extract_kapture --kapture-root ${WORKING_DIR}/${DATASET}/map_plus_validation/ --checkpoint dirtorch/data/Resnet101-AP-GeM-LM18.pt --gpu 0
+  ${PYTHONBIN} -m dirtorch.extract_kapture --kapture-root ${WORKING_DIR}/${DATASET}/map_plus_testing/ --checkpoint dirtorch/data/Resnet101-AP-GeM-LM18.pt --gpu 0
   # move to right location
   mkdir -p ${WORKING_DIR}/${DATASET}/global_features/Resnet101-AP-GeM-LM18/global_features
-  mv ${WORKING_DIR}/${DATASET}/map_plus_validation/reconstruction/global_features/Resnet101-AP-GeM-LM18/* ${WORKING_DIR}/${DATASET}/global_features/Resnet101-AP-GeM-LM18/global_features/
-  rm -rf ${WORKING_DIR}/${DATASET}/map_plus_validation/reconstruction/global_features/Resnet101-AP-GeM-LM18
+  mv ${WORKING_DIR}/${DATASET}/map_plus_testing/reconstruction/global_features/Resnet101-AP-GeM-LM18/* ${WORKING_DIR}/${DATASET}/global_features/Resnet101-AP-GeM-LM18/global_features/
+  rm -rf ${WORKING_DIR}/${DATASET}/map_plus_testing/reconstruction/global_features/Resnet101-AP-GeM-LM18
 
   # 4) Extract local features (we will use R2D2 here)
   cd ${WORKING_DIR}/r2d2
-  ${PYTHONBIN} extract_kapture.py --model models/r2d2_WASF_N8_big.pt --kapture-root ${WORKING_DIR}/${DATASET}/map_plus_validation/ --top-k 20000 --max-size 1024
+  ${PYTHONBIN} extract_kapture.py --model models/r2d2_WASF_N8_big.pt --kapture-root ${WORKING_DIR}/${DATASET}/map_plus_testing/ --top-k 20000 --max-size 1024
   # move to right location
   mkdir -p ${WORKING_DIR}/${DATASET}/local_features/r2d2_WASF_N8_big/descriptors
-  mv ${WORKING_DIR}/${DATASET}/map_plus_validation/reconstruction/descriptors/r2d2_WASF_N8_big/* ${WORKING_DIR}/${DATASET}/local_features/r2d2_WASF_N8_big/descriptors/
+  mv ${WORKING_DIR}/${DATASET}/map_plus_testing/reconstruction/descriptors/r2d2_WASF_N8_big/* ${WORKING_DIR}/${DATASET}/local_features/r2d2_WASF_N8_big/descriptors/
   mkdir -p ${WORKING_DIR}/${DATASET}/local_features/r2d2_WASF_N8_big/keypoints
-  mv ${WORKING_DIR}/${DATASET}/map_plus_validation/reconstruction/keypoints/r2d2_WASF_N8_big/* ${WORKING_DIR}/${DATASET}/local_features/r2d2_WASF_N8_big/keypoints/
+  mv ${WORKING_DIR}/${DATASET}/map_plus_testing/reconstruction/keypoints/r2d2_WASF_N8_big/* ${WORKING_DIR}/${DATASET}/local_features/r2d2_WASF_N8_big/keypoints/
 
   # 5) mapping pipeline
   LOCAL=r2d2_WASF_N8_big
@@ -95,7 +95,7 @@ for SCENE in scene01 scene02 scene03 scene04 scene05 scene06 scene07 scene08 sce
   # 6) localization pipeline
   kapture_pipeline_localize.py -v debug -f \
     -i ${WORKING_DIR}/${DATASET}/mapping \
-    --query ${WORKING_DIR}/${DATASET}/validation \
+    --query ${WORKING_DIR}/${DATASET}/testing \
     -kpt ${WORKING_DIR}/${DATASET}/local_features/${LOCAL}/keypoints \
     -desc ${WORKING_DIR}/${DATASET}/local_features/${LOCAL}/descriptors \
     -gfeat ${WORKING_DIR}/${DATASET}/global_features/${GLOBAL}/global_features \
