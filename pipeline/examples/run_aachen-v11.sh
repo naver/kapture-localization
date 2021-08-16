65353e8e6763895f0a64b924ab499d3d3e398ab5
Yohann CABON <yohanncabon@gmail.com>
2021-08-16 09:27:29 +0200

    r2d2 extract_kapture.py --top-k instead of max-keypoints

--- pipeline/examples/run_aachen-v11.sh
+++ pipeline/examples/run_aachen-v11.sh
@@ -67,7 +67,7 @@ rm -rf ${WORKING_DIR}/${DATASET}/map_plus_query/reconstruction/global_features/R
 
 # 5) Extract local features (we will use R2D2 here)
 cd ${WORKING_DIR}/r2d2
-${PYTHONBIN} extract_kapture.py --model models/r2d2_WASF_N8_big.pt --kapture-root ${WORKING_DIR}/${DATASET}/map_plus_query/ --min-scale 0.3 --min-size 128 --max-scale 9999 --max-keypoints ${KPTS}
+${PYTHONBIN} extract_kapture.py --model models/r2d2_WASF_N8_big.pt --kapture-root ${WORKING_DIR}/${DATASET}/map_plus_query/ --min-scale 0.3 --min-size 128 --max-scale 9999 --top-k ${KPTS}
 # move to right location
 mkdir -p ${WORKING_DIR}/${DATASET}/local_features/r2d2_WASF_N8_big/descriptors
 mv ${WORKING_DIR}/${DATASET}/map_plus_query/reconstruction/descriptors/r2d2_WASF_N8_big/* ${WORKING_DIR}/${DATASET}/local_features/r2d2_WASF_N8_big/descriptors/
